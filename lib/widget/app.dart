db954d184330f91efa621fcb9b1c920c7c81e5dc
aoisensi <me@aoisensi.info>
2025-03-12 15:16:00 +0900

    🎨 タイトル変更

--- lib/widget/app.dart
+++ lib/widget/app.dart
@@ -11,7 +11,7 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Riverpod Counter',
+      title: 'SkySoda',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
